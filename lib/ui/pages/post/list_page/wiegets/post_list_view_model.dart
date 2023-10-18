5e3a2ae04a1b2c278a67fbbf4b0caf0e85222fc1
muaga <more9494@naver.com>
2023-10-18 12:54:44 +0900

    게시물 등록 통신

--- lib/ui/pages/post/list_page/wiegets/post_list_view_model.dart
+++ lib/ui/pages/post/list_page/wiegets/post_list_view_model.dart
@@ -1,15 +1,17 @@
 // view-model에서 응답받는 데이터의 타입이 1개의 객체인 지 List인 지 잘 알아야 한다.
 
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
 import 'package:flutter_blog/data/model/post.dart';
 import 'package:flutter_blog/data/provider/session_provider.dart';
 import 'package:flutter_blog/data/repository/post_respository.dart';
+import 'package:flutter_blog/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// 1. 창고 데이터
 class PostListModel {
   List<Post> posts;
-
   PostListModel(this.posts);
 }
 
@@ -17,6 +19,7 @@ class PostListModel {
 class PostListViewModel extends StateNotifier<PostListModel?> {
   PostListViewModel(super._state, this.ref); // super._state = PostListModel 타입
 
+  final mContext = navigatorKey.currentContext;
   Ref ref;
 
   // state값이 변경되면 re-build가 되도록 알린다.
@@ -26,6 +29,31 @@ class PostListViewModel extends StateNotifier<PostListModel?> {
     ResponseDTO responseDTO =
         await PostRepository().fetchPostList(sessionUser.jwt!);
     state = PostListModel(responseDTO.data);
+  }
+
+  Future<void> notifyAdd(PostSaveReqDTO dto) async {
+    SessionUser sessionUser = ref.read(sessionProvider);
+
+    ResponseDTO responseDTO =
+        await PostRepository().fetchPost(sessionUser.jwt!, dto);
+    // fetchSave할 때, token과 body데이터를 넘겨줘야 한다.
+
+    if (responseDTO.code == 1) {
+      /// dynamic(post) -> 다운 캐스팅
+      Post newPost = responseDTO.data as Post;
+
+      /// 상태값 추가(List 추가)
+      List<Post> newPosts = [newPost, ...state!.posts];
+
+      /// 뷰모델(창고) 상태 갱신 완료 -> 구독자(postListPage)는 자동 갱신
+      state = PostListModel(newPosts);
+
+      /// 해당 페이지 삭제
+      Navigator.pop(mContext!);
+    } else {
+      ScaffoldMessenger.of(mContext!).showSnackBar(
+          SnackBar(content: Text("게시물 작성 실패 : ${responseDTO.msg}")));
+    }
   }
 }
 
--- lib/ui/pages/post/write_page/widgets/post_write_form.dart
+++ lib/ui/pages/post/write_page/widgets/post_write_form.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/size.dart';
 import 'package:flutter_blog/_core/utils/validator_util.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
+import 'package:flutter_blog/ui/pages/post/list_page/wiegets/post_list_view_model.dart';
 import 'package:flutter_blog/ui/widgets/custom_elavated_button.dart';
 import 'package:flutter_blog/ui/widgets/custom_text_area.dart';
 import 'package:flutter_blog/ui/widgets/custom_text_form_field.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class PostWriteForm extends StatelessWidget {
+class PostWriteForm extends ConsumerWidget {
   final _formKey = GlobalKey<FormState>();
   final _title = TextEditingController();
   final _content = TextEditingController();
@@ -13,7 +17,7 @@ class PostWriteForm extends StatelessWidget {
   PostWriteForm({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Form(
       key: _formKey,
       child: ListView(
@@ -32,8 +36,15 @@ class PostWriteForm extends StatelessWidget {
           const SizedBox(height: largeGap),
           CustomElevatedButton(
             text: "글쓰기",
+            // 글을 쓰면 postListviewmodel의 상태를 바꿀 것이다.
+            // 그래서 postListviewmodel을 새로 만들 필요 없고, 기존 view 모델에 접근해야 한다.
             funPageRoute: () async {
               if (_formKey.currentState!.validate()) {
+                Logger().d("title : ${_title.text}");
+                Logger().d("content : ${_content.text}");
+                PostSaveReqDTO dto =
+                    PostSaveReqDTO(title: _title.text, content: _content.text);
+                ref.read(postListProvider.notifier).notifyAdd(dto);
               }
             },
           ),
